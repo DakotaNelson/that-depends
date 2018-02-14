290172a0954837a39943c83bf0428d01ad4daa13
Dakota Nelson <dakota.w.nelson@gmail.com>
2018-02-13 21:25:12 -0800

    initial commit

---
+++ that-depends/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace that_depends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
